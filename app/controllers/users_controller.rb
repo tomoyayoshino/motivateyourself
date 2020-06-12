3aa49d900a6e70bb70554bfcd2b1214fd0c49b40
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-12 18:15:02 +0900

    edit post.user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @name = current_user.name
-    @posts = current_user.posts.order("created_at DESC")
+    user = User.find(params[:id])
+    @name = user.name
+    @posts = user.posts.order("created_at DESC")
   end
 end
