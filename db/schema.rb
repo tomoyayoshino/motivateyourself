ea4a1e9edd0912d41a0dee925506062b61e5e21d
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-13 18:43:38 +0900

    edit column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_021324) do
+ActiveRecord::Schema.define(version: 2020_06_13_094134) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 2020_06_13_021324) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.integer "likes_count"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
