a72a0d892cb05fd742e14ef8b7e381c9aa42e931
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-15 18:06:02 +0900

    add index to tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,9 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
+    if params[:tag_name]
+      @posts = Post.tagged_with("#{params[:tag_name]}").order("created_at DESC")
+    end
   end
   
   def new
