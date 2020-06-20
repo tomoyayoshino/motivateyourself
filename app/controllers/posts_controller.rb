07cda12f1f893148ba1001362096235de386be28
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-20 15:37:46 +0900

    add search form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
 
   def index
     @posts = Post.includes(:user).order("created_at DESC")
@@ -37,6 +37,10 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments.includes(:user)
+  end
+
+  def search
+    @posts = Post.search(params[:keyword]).order("created_at DESC")
   end
 
   private
