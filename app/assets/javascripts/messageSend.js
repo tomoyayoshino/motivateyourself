c7e55f183753727958d8b5bf8f85b1871e2e84bf
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-20 11:42:27 +0900

    add js to dm

--- app/assets/javascripts/messageSend.js
+++ app/assets/javascripts/messageSend.js
@@ -1,56 +1,6 @@
 $(function(){
-  let current_user = gon.current_user;
   function buildHTML(m){
     if (m.image){
-      if (m.user == current_user){
-        let html = 
-        `<div class="MessageBox" data-message-id=${m.id}>
-          <div class="MessageBox__current">
-            <div class="MessageBox__current--flex">
-              <div class="MessageBox__current--date">
-                ${m.created_at}
-              </div>
-              <div class="MessageBox__current--parent">
-                <a href="${m.image}" 'data-lightbox': ${m.image}>
-                  <img src="${m.image}" class: 'MessageBox--image' width='66' height='50'>
-                </a>
-                <p>
-                  ${m.content}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>`
-        return html;
-      } else {
-        let html =
-        `<div class="MessageBox" data-message-id=${m.id}>
-          <div class="MessageBox__another">
-            <div class="MessageBox__another--parent">
-              <div class="MessageBox__another--flex">
-                <img src=${m.user.image.url}  width='50' height='50'>
-                <div class="MessageBox__another--contents">
-                  <a href="/users/${m.user.id}">
-                    ${m.user.name}さん
-                  </a>
-                  <p>
-                    ${m.content}
-                  </p>
-                </div>
-                <a href="${m.image}" 'data-lightbox': ${m.image} >
-                  <img src="${m.image}" class: 'MessageBox--image' width='66' height='50'>
-                </a>
-              </div>
-              <div class="MessageBox__current--date">
-                ${m.created_at}
-              </div>
-            </div>
-          </div>
-        </div>`
-      return html;
-      }
-    } else {
-      if (m.user == current_user){
       let html = 
       `<div class="MessageBox" data-message-id=${m.id}>
         <div class="MessageBox__current">
@@ -59,6 +9,9 @@ $(function(){
               ${m.created_at}
             </div>
             <div class="MessageBox__current--parent">
+              <a href="${m.image}" rel="lightbox">
+                <img src="${m.image}" class: 'MessageBox--image' width='66' height='50'>
+              </a>
               <p>
                 ${m.content}
               </p>
@@ -67,30 +20,23 @@ $(function(){
         </div>
       </div>`
       return html;
-      } else {
-      let html =
-      `<div class="MessageBox" data-message-id=${m.id}>
-        <div class="MessageBox__another">
-          <div class="MessageBox__another--parent">
-            <div class="MessageBox__another--flex">
-              <img src=${m.user.image} width='50' height='50'>
-              <div class="MessageBox__another--contents">
-                <a href="/users/${m.user.id}">
-                  ${m.user.name}さん
-                </a>
-                <p>
-                  ${m.content}
-                </p>
-              </div>
-            </div>
-            <div class="MessageBox__current--date">
-              ${m.created_at}
-            </div>
+    } else {
+    let html = 
+    `<div class="MessageBox" data-message-id=${m.id}>
+      <div class="MessageBox__current">
+        <div class="MessageBox__current--flex">
+          <div class="MessageBox__current--date">
+            ${m.created_at}
+          </div>
+          <div class="MessageBox__current--parent">
+            <p>
+              ${m.content}
+            </p>
           </div>
         </div>
-      </div>`
-      return html;
-      }
+      </div>
+    </div>`
+    return html;
     }
   }
 
