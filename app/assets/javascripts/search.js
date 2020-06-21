a2bffc93ca74074f0da283bbe3943c423be21613
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-21 09:57:37 +0900

    add incremental serach html

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,13 +2,80 @@ $(function() {
   let search_list = $(".Timeline__main");
 
   function appendPost(post){
-    if(post.user_sign_in && post.user_sign_in.id == post.user_id){
+    if(post.image){
       let html =
-      `c`
+      `<div class='ContentBox'>
+        <div class='ContentBox__info'>
+          <div class='ContentBox__info--image'>
+            <img src="${post.icon}" width='50' height '50'>
+          </div>
+          <div class='ContentBox__info--name'>
+            <a href="/users/${post.user_id}">
+              ${post.name}
+            </a>
+          </div>
+          <div class='ContentBox__info--follow'></div>
+        </div>
+        <div class='ContentBox__contents'>
+          <div class='ContentBox__contents--text'>
+            <p>${post.content}</p>
+          </div>
+          <div class='ContentBox__contents--image'>
+            <a href="${post.image}" rel="lightbox">
+              <img src="${post.image}" class: 'ContentBox__contents--image'>
+            </a>
+          </div>
+        </div>
+        <div class='ContentBox__tags'>
+        </div>
+        <div class='ContentBox__actionBar'>
+          <div class='ContentBox__actionBar--date'>
+            ${post.created_at}
+          </div>
+          <div>
+          </div>
+          <div class='ContentBox__actionBar--detail'>
+            <a href="posts/${post.id}">
+              詳細
+            </a>
+          </div>
+        </div>
+      </div>`
       search_list.append(html);
     } else {
       let html =
-      `a`
+      `<div class='ContentBox'>
+        <div class='ContentBox__info'>
+          <div class='ContentBox__info--image'>
+            <img src="${post.icon}" width='50' height '50'>
+          </div>
+          <div class='ContentBox__info--name'>
+            <a href="/users/${post.user_id}">
+              ${post.name}
+            </a>
+          </div>
+          <div class='ContentBox__info--follow'></div>
+        </div>
+        <div class='ContentBox__contents'>
+          <div class='ContentBox__contents--text'>
+            <p>${post.content}</p>
+          </div>
+        </div>
+        <div class='ContentBox__tags'>
+        </div>
+        <div class='ContentBox__actionBar'>
+          <div class='ContentBox__actionBar--date'>
+            ${post.created_at}
+          </div>
+          <div>
+          </div>
+          <div class='ContentBox__actionBar--detail'>
+            <a href="posts/${post.id}">
+              詳細
+            </a>
+          </div>
+        </div>
+      </div>`
       search_list.append(html);
     }
   }
