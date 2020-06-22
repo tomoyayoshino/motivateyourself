4c9c37e3be5600217ad6a018493433564b74b939
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-22 09:41:46 +0900

    change redirect

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
 class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
+    user = User.find(params[:user_id])
     follow.save
-    redirect_to follows_user_path(current_user)
+    redirect_to user_path(user)
   end
 
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    user = User.find(params[:user_id])
     follow.destroy
-    redirect_to follows_user_path(current_user)
+    redirect_to user_path(user)
   end
 end
