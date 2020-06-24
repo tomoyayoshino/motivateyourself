1627a6becda5f2e446c00372b69994755f493ba2
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-24 19:08:46 +0900

    test task controller

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe TasksController, type: :controller do
+  let(:user) { create(:user) }
+  before do
+    login user
+  end
+
+  describe 'GET #new' do
+    it "newアクションで正しくビューに遷移すること" do
+      get :new
+      expect(response).to render_template :new
+    end
+  end
+  describe 'GET #edit' do
+    it "@taskの値を確かめる" do
+      task = create(:task)
+      get :edit, params: { id: task }
+      expect(assigns(:task)).to eq task
+    end
+    it "editアクションで正しくビューに遷移すること" do
+      task = create(:task)
+      get :edit, params: { id: task }
+      expect(response).to render_template :edit
+    end
+  end
+end
+
+
