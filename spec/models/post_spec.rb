8c73b898f6ad511d85f0ce33abe39dc6fb986f1e
tomoyayoshino <t.yoshino0331@gmail.com>
2020-06-24 16:40:05 +0900

    test post model

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+describe Post do
+  describe '#create' do
+    it "contentが空では投稿できないこと" do
+      post = build(:post, content: "")
+      post.valid?
+      expect(post.errors[:content]).to include("を入力してください")
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,40 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
-    it "nameにバリデーションが適用されていること" do
+    it "name,email,image,password,password_confirmationがあれば登録できること" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+    it "nameが空では登録できないこと" do
       user = build(:user, name: "")
       user.valid?
       expect(user.errors[:name]).to include("を入力してください")
     end
-    it "emailにバリデーションが適用されていること" do
+    it "emailが空では登録できないこと" do
       user = build(:user, email: "")
       user.valid?
       expect(user.errors[:email]).to include("を入力してください")
     end
-    it "imageにバリデーションが適用されていること" do
+    it "imageが空では登録できないこと" do
       user = build(:user, image: "")
       user.valid?
       expect(user.errors[:image]).to include("を入力してください")
     end
-    it "passwordにバリデーションが適用されていること" do
+    it "passwordが空では登録できないこと" do
       user = build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include("を入力してください")
     end
-    it "passwordが存在してもpassword_confirmationが必須であること" do
+    it "passwordが存在してもpassword_confirmationが空では登録できないこと" do
       user = build(:user, password_confirmation: "")
       user.valid?
       expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+    end
+    it "重複したnameでは登録できないこと" do
+      user = create(:user)
+      another_user = build(:user, name: user.name)
+      another_user.valid?
+      expect(another_user.errors[:name]).to include("はすでに存在します")
     end
     it "重複したemailでは登録できないこと" do
       user = create(:user)
@@ -41,14 +51,14 @@ describe User do
       user.valid?
       expect(user.errors[:password]).to include("は6文字以上で入力してください")
     end
+    it "nameが10文字以下なら登録できること" do
+      user = build(:user, name: "abcdefghij")
+      expect(user).to be_valid
+    end
     it "nameが11文字以上だと登録できないこと" do
       user = build(:user, name: "abcdefghijk")
       user.valid?
       expect(user.errors[:name]).to include("は10文字以内で入力してください")
-    end
-    it "nameが10文字以下なら登録できること" do
-      user = build(:user, name: "abcdefghij")
-      expect(user).to be_valid
     end
   end
 end
