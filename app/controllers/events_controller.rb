a8db9662d7f5c7213a12858d8eb79a9f5dd9a90c
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-18 10:35:26 +0900

    add event show

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :move_to_index
+  before_action :set_event, only: [:edit, :show]
 
   def index
     @events = Event.includes(:user).order("created_at DESC").limit(255)
@@ -25,7 +26,6 @@ class EventsController < ApplicationController
   end
 
   def edit
-    @event = Event.find(params[:id])
   end
 
   def update
@@ -34,11 +34,8 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
-  # def show
-  #   @post = Post.find(params[:id])
-  #   @comment = Comment.new
-  #   @comments = @post.comments.includes(:user)
-  # end
+  def show
+  end
 
   # def search
   #   @posts = Post.search(params[:keyword]).order("created_at DESC").limit(255)
@@ -52,6 +49,10 @@ class EventsController < ApplicationController
 
   def event_params
     params.require(:event).permit(:title, :description, :place, :day, :start, :end).merge(user_id: current_user.id)
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 
   def move_to_index
