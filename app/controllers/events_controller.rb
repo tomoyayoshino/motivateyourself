fe6ba1862af9945a916fdce720d4c77cf6381917
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-18 10:29:14 +0900

    add edit event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -24,15 +24,15 @@ class EventsController < ApplicationController
     redirect_to events_path
   end
 
-  # def edit
-  #   @post = Post.find(params[:id])
-  # end
+  def edit
+    @event = Event.find(params[:id])
+  end
 
-  # def update
-  #   post = Post.find(params[:id])
-  #   post.update(post_params)
-  #   redirect_to root_path
-  # end
+  def update
+    event = Event.find(params[:id])
+    event.update(event_params)
+    redirect_to events_path
+  end
 
   # def show
   #   @post = Post.find(params[:id])
