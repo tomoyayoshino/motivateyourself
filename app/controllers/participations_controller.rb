7a98c47f1f4f9b070d2978cb5215c254eaa7157a
tomoyayoshino <t.yoshino0331@gmail.com>
2020-07-24 07:01:31 +0900

    edit participation button

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -4,11 +4,13 @@ class ParticipationsController < ApplicationController
   def create
     participation = current_user.participations.build(event_id: params[:event_id])
     participation.save
+    redirect_to event_path(@event.id)
   end
 
   def destroy
     participation = Participation.find_by(event_id: params[:event_id], user_id: current_user.id)
     participation.destroy
+    redirect_to event_path(@event.id)
   end
 
   private
